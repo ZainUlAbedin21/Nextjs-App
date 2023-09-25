d5fc7fe8fb58847d3bbc251c8fd7bd3f45ffc24a
ZainUlAbedin21 <zain.ul.abdin2121@gmail.com>
2023-09-25 23:16:06 +0500

    HERO PART

---
+++ components/Hero.tsx
@@ -0,0 +1,43 @@
+"use client"
+import React from 'react'
+import { CustomButton } from '.'
+import Image from 'next/image'
+
+const Hero = () => {
+
+  const handleScroll=()=>{
+
+  }
+
+  return (
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+            Find Your Dream Mobile order quickly
+            </h1>
+            <p className='hero__subtiltle'>
+                Streamline your Mobile Order experience with our effortless process.
+            </p>
+
+          <CustomButton
+          title="Explore Phones"
+          containerStyles="bg-primary-blue
+          text-white rounded-full mt-10"
+          handleClick={handleScroll}
+          />
+
+        </div>
+<div className="hero__image-conatainer">
+<div className="hero__image">
+<Image src="/hero.png" alt="hero"
+fill className="object-contain"
+/>
+<div className='hero__image-overlay'/>
+</div>
+</div>
+
+    </div>
+  )
+}
+
+export default Hero
